695f6fbd212ddd56f3b2770475977064936d2d49
Mari091 <mary.solanom@gmail.com>
2025-03-09 13:55:37 -0600

    Modelo y Negocio de Departamentos.

---
+++ Modelos/DepartamentoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class DepartamentoModel
+    {
+        [Display(Name = "Id Departamento")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdDepartamento { get; set; }  
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Nombre del Departamento")]
+        public string Departamento { get; set; }
+
+    } //Fin clase
+
+} // Fin space
---
+++ Negocios/DepartamentosNegocio.cs
@@ -0,0 +1,99 @@
+﻿using Datos;
+using Microsoft.Data.SqlClient;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Negocios
+{
+    public class DepartamentosNegocio
+    {
+        //***************** VARIABLES *******************
+        private readonly SQLServerContext_Datos _datos;
+
+        public DepartamentoNegocios(SQLServerContext_Datos datos)
+        {
+            _datos = datos;
+        }
+
+        // Consultar un departamento por ID
+        public DepartamentoModel ConsultarDepartamentoID(int id)
+        {
+            List<SqlParameter> parametros = new()
+        {
+            new SqlParameter("@idDepartamento", id)
+        };
+
+            DataTable dt = _datos.EjecutarSQLconSP_DT("sp_ConsultarDepartamentoID", parametros);
+
+            if (dt.Rows.Count == 0)
+                return null;
+
+            DataRow row = dt.Rows[0];
+
+            return new DepartamentoModel
+            {
+                IdDepartamento = Convert.ToInt32(row["idDepartamento"]),
+                Departamento = row["Departamento"].ToString()
+            };
+        }//fin ConsultarDepartamentoID
+
+        // Listar todos los departamentos
+        public List<DepartamentoModel> ListarDepartamentos()
+        {
+            DataTable dt = _datos.EjecutarSQLconSP_DT("sp_ListarDepartamentos", new List<SqlParameter>());
+            List<DepartamentoModel> lista = new();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                lista.Add(new DepartamentoModel
+                {
+                    IdDepartamento = Convert.ToInt32(row["idDepartamento"]),
+                    Departamento = row["Departamento"].ToString()
+                });
+            }
+
+            return lista;
+        }//fin ListarDepartamentos
+
+        // Crear un nuevo departamento
+        public void CrearDepartamento(DepartamentoModel departamento)
+        {
+            List<SqlParameter> parametros = new()
+        {
+            new SqlParameter("@Departamento", departamento.Departamento)
+        };
+
+            _datos.EjecutarSQLconSP_Void("sp_CrearDepartamento", parametros);
+        }//fin CrearDepartamento 
+
+        // Modificar un departamento existente
+        public void ModificarDepartamento(DepartamentoModel departamento)
+        {
+            List<SqlParameter> parametros = new()
+        {
+            new SqlParameter("@idDepartamento", departamento.IdDepartamento),
+            new SqlParameter("@Departamento", departamento.Departamento)
+        };
+
+            _datos.EjecutarSQLconSP_Void("sp_ModificarDepartamento", parametros);
+        }//fin ModificarDepartamento
+
+        // Eliminar un departamento por ID
+        public void EliminarDepartamento(int id)
+        {
+            List<SqlParameter> parametros = new()
+        {
+            new SqlParameter("@idDepartamento", id)
+        };
+
+            _datos.EjecutarSQLconSP_Void("sp_EliminarDepartamento", parametros);
+        }//fin EliminarDepartamento
+
+    } //Fin Clase
+} //Fin Space
