6405320435e7d9887657d5c55f070e2b93f66f20
andrey quesada gonzalez <andreyq149@gmail.com>
2025-03-09 16:21:31 -0600

    Ventana sobre la administracion de los departamentos hecha

--- SEDEP/Controllers/MantenimientosController.cs
+++ SEDEP/Controllers/MantenimientosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Modelos;
 
 namespace SEDEP.Controllers
 {
@@ -7,6 +8,27 @@ namespace SEDEP.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ManteniDepartamentos()
+        {
+            var departamentos = new List<DepartamentoModel>
+            {
+                new DepartamentoModel { IdDepartamento = 1, Departamento = "Recursos Humanos" },
+                new DepartamentoModel { IdDepartamento = 2, Departamento = "IT" }
+            };
+            return View(departamentos);
+        }
+
+        public IActionResult CreaDepartamento()
+        {
+            return View(new DepartamentoModel());
+        }
+
+        public IActionResult EditaDepartamento(int id)
+        {
+            var departamento = new DepartamentoModel { IdDepartamento = id, Departamento = "IT" };
+            return View(departamento);
         }
     }
 }
