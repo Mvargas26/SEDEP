386fb667288b88a3009d4ae0e7f531381c449dc0
Yordan <yordan10_pv@hotmail.com>
2025-03-10 14:01:07 -0600

    Se ha creado la interfaz del login

---
+++ SEDEP/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDEP.Models;
+
+namespace SEDEP.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Simulación de autenticación
+            if (model.Usuario == "admin" && model.Password == "1234")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Usuario o contraseña incorrectos.");
+            return View(model);
+        }
+    }
+}
---
+++ SEDEP/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEDEP.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
