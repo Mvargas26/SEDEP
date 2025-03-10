357d886ec83789fcd188c4e36cdf997661ab0eab
Yordan <yordan10_pv@hotmail.com>
2025-03-10 09:48:10 -0600

    Se ha creado CRUD Puesto

---
+++ Negocios/PuestoNegocio.cs
@@ -0,0 +1,97 @@
+﻿using Datos;
+using Microsoft.Data.SqlClient;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class PuestosNegocio
+    {
+        private readonly SQLServerContext_Datos _contexto;
+
+        public PuestosNegocio()
+        {
+            _contexto = new SQLServerContext_Datos();
+        }
+
+        //Obtener todos
+        public List<PuestoModel> ObtenerTodosLosPuestos()
+        {
+            List<PuestoModel> listaPuestos = new();
+            DataTable dt = _contexto.EjecutarSqlDirecto_DT("SELECT * FROM Puesto");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                listaPuestos.Add(new PuestoModel
+                {
+                    idPuesto = Convert.ToInt32(row["idPuesto"]),
+                    Puesto = row["Puesto"].ToString()
+                });
+            }
+
+            return listaPuestos;
+        }
+
+        // Obtener por ID
+        public PuestoModel ObtenerPuestoPorId(int id)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@idPuesto", id)
+            };
+
+            DataTable dt = _contexto.EjecutarSQLconSP_DT("sp_ObtenerPuesto", parametros);
+
+            if (dt.Rows.Count > 0)
+            {
+                return new PuestoModel
+                {
+                    idPuesto = Convert.ToInt32(dt.Rows[0]["idPuesto"]),
+                    Puesto = dt.Rows[0]["Puesto"].ToString()
+                };
+            }
+
+            return null;
+        }
+
+        // Agregar
+        public void AgregarPuesto(PuestoModel puesto)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@Puesto", puesto.Puesto)
+            };
+
+            _contexto.EjecutarSQLconSP_Void("sp_InsertarPuesto", parametros);
+        }
+
+        // Actualizar
+        public void ActualizarPuesto(PuestoModel puesto)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@idPuesto", puesto.idPuesto),
+                new SqlParameter("@Puesto", puesto.Puesto)
+            };
+
+            _contexto.EjecutarSQLconSP_Void("sp_ActualizarPuesto", parametros);
+        }
+
+        // Eliminar
+        public void EliminarPuesto(int id)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@idPuesto", id)
+            };
+
+            _contexto.EjecutarSQLconSP_Void("sp_EliminarPuesto", parametros);
+        }
+    }
+
+}
