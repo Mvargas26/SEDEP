c6de33167098740a8e84ba6750583728a03b4beb
Michael Vargas S. <michaelvargas47@hotmail.com>
2025-03-16 15:44:24 -0600

    metodos funcionarios listos hasta el backend

---
+++ Modelos/FuncionarioModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class FuncionarioModel
+    {
+        [Key]
+        [Column(TypeName = "VARCHAR(20)")]
+        public string Cedula { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Apellido1 { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Apellido2 { get; set; }
+
+        [Column(TypeName = "VARCHAR(150)")]
+        public string Correo { get; set; }
+
+        [Column(TypeName = "VARCHAR(255)")]
+        public string Password { get; set; }
+
+        public int IdDepartamento { get; set; }
+        public int IdRol { get; set; }
+        public int IdPuesto { get; set; }
+        public int IdEstadoFuncionario { get; set; }
+
+        // Relaciones
+        //[ForeignKey("IdDepartamento")]
+        //public virtual Departamento Departamento { get; set; }
+
+        //[ForeignKey("IdRol")]
+        //public virtual Rol Rol { get; set; }
+
+        //[ForeignKey("IdPuesto")]
+        //public virtual Puesto Puesto { get; set; }
+
+        //[ForeignKey("IdEstadoFuncionario")]
+        //public virtual EstadoFuncionario EstadoFuncionario { get; set; }
+
+    }//public class
+}//fin space
---
+++ Negocios/FuncionarioNegocios.cs
@@ -0,0 +1,134 @@
+﻿using Datos;
+using Microsoft.Data.SqlClient;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    class FuncionarioNegocios
+    {
+        //***************** VARIABLES *******************
+        SQLServerContext_Datos objDatos = new SQLServerContext_Datos();
+
+
+        public FuncionarioModel ConsultarFuncionarioID(string cedula)
+        {
+            List<SqlParameter> parametros = new()
+    {
+        new SqlParameter("@cedula", cedula)
+    };
+
+            DataTable dt = objDatos.EjecutarSQLconSP_DT("sp_ConsultarFuncionarioID", parametros);
+
+            if (dt.Rows.Count == 0)
+                return null;
+
+            DataRow row = dt.Rows[0];
+
+            return new FuncionarioModel
+            {
+                Cedula = row["cedula"].ToString(),
+                Nombre = row["nombre"].ToString(),
+                Apellido1 = row["apellido1"].ToString(),
+                Apellido2 = row["apellido2"].ToString(),
+                Correo = row["correo"].ToString(),
+                Password = row["password"].ToString(),
+                IdDepartamento = Convert.ToInt32(row["idDepartamento"]),
+                IdRol = Convert.ToInt32(row["idRol"]),
+                IdPuesto = Convert.ToInt32(row["idPuesto"]),
+                IdEstadoFuncionario = Convert.ToInt32(row["idEstadoFuncionario"])
+            };
+        }
+
+
+        public List<FuncionarioModel> ListarFuncionarios()
+        {
+            List<SqlParameter> parametros = new()
+        {
+            new SqlParameter("@Accion", "SELECT")
+        };
+
+            DataTable dt = objDatos.EjecutarSQLconSP_DT("[adm].[sp_CrudFuncionarios]", parametros);
+            List<FuncionarioModel> lista = new();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                lista.Add(new FuncionarioModel
+                {
+                    Cedula = row["cedula"].ToString(),
+                    Nombre = row["nombre"].ToString(),
+                    Apellido1 = row["apellido1"].ToString(),
+                    Apellido2 = row["apellido2"].ToString(),
+                    Correo = row["correo"].ToString(),
+                    Password = row["password"].ToString(),
+                    IdDepartamento = Convert.ToInt32(row["idDepartamento"]),
+                    IdRol = Convert.ToInt32(row["idRol"]),
+                    IdPuesto = Convert.ToInt32(row["idPuesto"]),
+                    IdEstadoFuncionario = Convert.ToInt32(row["idEstadoFuncionario"])
+                });
+            }
+
+            return lista;
+        }
+
+
+        public void CrearFuncionario(FuncionarioModel funcionarioNuevo)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@Accion", "INSERT"),
+                new SqlParameter("@Cedula", funcionarioNuevo.Cedula),
+                new SqlParameter("@Nombre", funcionarioNuevo.Nombre),
+                new SqlParameter("@Apellido1", funcionarioNuevo.Apellido1),
+                new SqlParameter("@Apellido2", funcionarioNuevo.Apellido2 ?? (object)DBNull.Value),
+                new SqlParameter("@Correo", funcionarioNuevo.Correo),
+                new SqlParameter("@Password", funcionarioNuevo.Password),
+                new SqlParameter("@IdDepartamento", funcionarioNuevo.IdDepartamento),
+                new SqlParameter("@IdRol", funcionarioNuevo.IdRol),
+                new SqlParameter("@IdPuesto", funcionarioNuevo.IdPuesto),
+                new SqlParameter("@IdEstadoFuncionario", funcionarioNuevo.IdEstadoFuncionario)
+            };
+
+            objDatos.EjecutarSQLconSP_Void("[adm].[sp_CrudFuncionarios]", parametros);
+        }//fin CrearFuncionario
+
+
+        public void ModificarFuncionario(FuncionarioModel funcionario)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@Accion", "UPDATE"),
+                new SqlParameter("@Cedula", funcionario.Cedula),
+                new SqlParameter("@Nombre", funcionario.Nombre ?? (object)DBNull.Value),
+                new SqlParameter("@Apellido1", funcionario.Apellido1 ?? (object)DBNull.Value),
+                new SqlParameter("@Apellido2", funcionario.Apellido2 ?? (object)DBNull.Value),
+                new SqlParameter("@Correo", funcionario.Correo ?? (object)DBNull.Value),
+                new SqlParameter("@Password", funcionario.Password ?? (object)DBNull.Value),
+                new SqlParameter("@IdDepartamento", funcionario.IdDepartamento) ,
+                new SqlParameter("@IdRol", funcionario.IdRol) ,
+                new SqlParameter("@IdPuesto", funcionario.IdPuesto) ,
+                new SqlParameter("@IdEstadoFuncionario", funcionario.IdEstadoFuncionario) 
+            };
+
+            objDatos.EjecutarSQLconSP_Void("[adm].[sp_CrudFuncionarios]", parametros);
+        }
+
+
+        public void EliminarFuncionario(string cedula)
+        {
+            List<SqlParameter> parametros = new()
+            {
+                new SqlParameter("@Accion", "DELETE"),
+                new SqlParameter("@Cedula", cedula)
+            };
+            objDatos.EjecutarSQLconSP_Void("[adm].[sp_CrudFuncionarios]", parametros);
+        }//fin EliminarFuncionario
+
+
+    }//fn class
+}//fn space
