bd8f37a2b0607394bb3f6f2f7f9eeb7065425f06
Michael Vargas S. <michaelvargas47@hotmail.com>
2025-03-17 20:32:53 -0600

    Revert "Validando entradas"
    This reverts commit 2c8eca6749846b874898fb64f8f624ea3b99a28a.

--- SEDEP/Controllers/AuthController.cs
+++ SEDEP/Controllers/AuthController.cs
@@ -1,29 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SEDEP.Models;
-using SEDEP.Datos;
-using System.Linq;
-using Org.BouncyCastle.Crypto.Generators.BCrypt.Net;
-using Org.BouncyCastle.Crypto.Generators;
-using SEDEP.Models;
 
 namespace SEDEP.Controllers
 {
     public class AuthController : Controller
     {
-        private readonly SQLServerContext _context;
-
-        public AuthController(SQLServerContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken] // Protección contra ataques CSRF
         public IActionResult Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
@@ -31,17 +18,13 @@ namespace SEDEP.Controllers
                 return View(model);
             }
 
-            // Buscar el usuario en la base de datos
-            var usuarioEncontrado = _context.Funcionarios.FirstOrDefault(u => u.Usuario == model.Usuario);
-
-            // Validar usuario y contraseña con hash
-            //if (usuarioEncontrado != null && BCrypt.Net.BCrypt.Verify(model.Password, usuarioEncontrado.password))
-            if(true)
+            // Simulación de autenticación
+            if (model.Usuario == "admin" && model.Password == "1234")
             {
                 return RedirectToAction("Index", "Home");
             }
 
-            ModelState.AddModelError(string.Empty, "Usuario o contraseña incorrectos.");
+            ModelState.AddModelError("", "Usuario o contraseña incorrectos.");
             return View(model);
         }
     }
