8325a74ce73688cef6b8efeb16f1d4c79c9bf61f
Yordan <yordan10_pv@hotmail.com>
2025-03-26 10:22:55 -0600

    Se le dirigirá al menú correspondiente de su rol si sus datos son correctos.

--- SEDEP/Controllers/AuthController.cs
+++ SEDEP/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SEDEP.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace SEDEP.Controllers
 {
@@ -18,11 +19,24 @@ namespace SEDEP.Controllers
                 return View(model);
             }
 
-            // Simulación de autenticación
+            // Simulación de autenticación con roles
             if (model.Usuario == "admin" && model.Password == "1234")
             {
-                TempData["SuccessMessage"] = "Inicio de sesión exitoso. Bienvenido.";
-                return RedirectToAction("Index", "Home");
+                TempData["SuccessMessage"] = "Inicio de sesión exitoso. Bienvenido, Administrador.";
+                HttpContext.Session.SetString("UserRole", "Administración");
+                return RedirectToAction("DashboardAdmin", "Home"); // Redirigir a la vista de administrador
+            }
+            else if (model.Usuario == "user" && model.Password == "1234")
+            {
+                TempData["SuccessMessage"] = "Inicio de sesión exitoso. Bienvenido, Usuario.";
+                HttpContext.Session.SetString("UserRole", "Jefatura");
+                return RedirectToAction("DashboardJefatura", "Home"); // Redirigir a la vista de jefatura
+            }
+            else if (model.Usuario == "user" && model.Password == "1234")
+            {
+                TempData["SuccessMessage"] = "Inicio de sesión exitoso. Bienvenido, Usuario.";
+                HttpContext.Session.SetString("UserRole", "SubAlterno");
+                return RedirectToAction("DashboardUser", "Home"); // Redirigir a la vista de usuario
             }
 
             ModelState.AddModelError("", "Error, Datos incorrectos.");
